b4f0ebae1349eb0caa20585853192b28755d7d02
MUSKU ARUN KUMAR <110256923+arun-4321@users.noreply.github.com>
2025-06-27 13:50:22 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull arunkumar4321/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 arunkumar4321/simple-python-flask-app  
+docker run -d -p 5000:5000 arunkumar4321/simple-python-flask-app   
